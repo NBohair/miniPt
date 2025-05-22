8cdec8ca68247362fc1026184dd868af582122e1
NBohair <nabil.bohair@um5r.ac.ma>
2025-05-23 00:08:49 +0100

    Update sidebare.php

--- partials/sidebare.php
+++ partials/sidebare.php
@@ -4,10 +4,7 @@
       <img src="images/logowhite.png" alt="DimaBuy Logo">
     </a>
   </div>
-  
-  <form method="post" action="logout.php" style="margin: 20px 0;">
-    <button type="submit" class="nav-btn">Log Out</button>
-  </form>
+
 
   <nav class="sidebar-nav">
     <a href="add_item.php"    class="nav-btn">Add Item</a>
@@ -15,5 +12,6 @@
     <a href="delete_item.php" class="nav-btn">Delete Item</a>
     <a href="users.php"       class="nav-btn">Users</a>
     <a href="orders.php"      class="nav-btn">View All Orders</a> 
+    <a href="logout.php"      class="nav-btn">Log out</a> 
   </nav>
 </aside>
